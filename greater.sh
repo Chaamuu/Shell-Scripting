d65d0422cf8d5f1484028968a86db7db9915d304
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-26 12:23:27 +0530

    Add files via upload

---
+++ greater.sh
@@ -0,0 +1,28 @@
+!/bin/sh
+
+
+echo "Enter a number: "
+read a
+
+# check
+if [ $a -gt 10 ]
+then
+  echo "Greater than 10."
+else
+  echo "Not greater than 10."
+fi
+
+
+!/bin/sh
+
+# take a numbers from the user
+echo "Enter a number: "
+read a
+
+# check
+if [ $a -lt 10 ]
+then
+  echo "Less than 10."
+else
+  echo "Not less than 10."
+fi
